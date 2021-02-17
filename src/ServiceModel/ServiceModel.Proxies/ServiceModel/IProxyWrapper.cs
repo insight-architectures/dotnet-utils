d5f225afb4300e123a4b752e5cdd6440989e923c
Renato Golia <renato.golia@insightarchitectures.com>
2021-02-17 14:15:37 +0100

    Added ServiceModel.Proxies project

---
+++ src/ServiceModel/ServiceModel.Proxies/ServiceModel/IProxyWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InsightArchitectures.Utilities.ServiceModel
+{
+
+    public interface IProxyWrapper<out TContract> :
+#if NETCOREAPP || NETSTANDARD2_1
+        IAsyncDisposable
+#elif NETFRAMEWORK || NETSTANDARD2_0
+        IDisposable
+#endif
+        where TContract : class
+    {
+        TContract Service { get; }
+    }
+}
