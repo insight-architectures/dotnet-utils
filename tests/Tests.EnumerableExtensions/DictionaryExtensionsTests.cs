f2ccc0da88f561b003e92a4e9fc9427bfb59eaf5
Renato Golia <renato.golia@insightarchitectures.com>
2022-06-22 14:49:44 +0200

    Added GetValueOrFallback and GetValueOrDefault to Dictionary

--- tests/Tests.EnumerableExtensions/DictionaryExtensionsTests.cs
+++ tests/Tests.EnumerableExtensions/DictionaryExtensionsTests.cs
@@ -90,4 +90,152 @@ public class DictionaryExtensionsTests
         [Test, CustomAutoData]
         public void Throws_is_list_is_null(KeyValuePair<string, string>[] testItems) => Assert.That(() => DictionaryExtensions.AddRange(null!, testItems), Throws.ArgumentNullException);
     }
+
+    [TestFixture]
+    [TestOf(nameof(DictionaryExtensions.GetValueOrFallback))]
+    public class GetValueOrFallback
+    {
+        [Test, CustomAutoData]
+        public void Returns_value_if_key_is_available_in_dictionary(string key, string value, string fallback)
+        {
+            var sut = new Dictionary<string, string>
+            {
+                [key] = value
+            };
+
+            var result = DictionaryExtensions.GetValueOrFallback(sut, key, fallback);
+
+            Assert.That(result, Is.EqualTo(value));
+        }
+
+        [Test, CustomAutoData]
+        public void Returns_fallback_value_if_key_is_not_available_in_dictionary(string key, string fallback)
+        {
+            var sut = new Dictionary<string, string>();
+
+            var result = DictionaryExtensions.GetValueOrFallback(sut, key, fallback);
+
+            Assert.That(result, Is.EqualTo(fallback));
+        }
+
+        [Test, CustomAutoData]
+        public void Returns_value_if_key_is_available(string key, string value, string fallback)
+        {
+            IDictionary<string, string> sut = new Dictionary<string, string>
+            {
+                [key] = value
+            };
+
+            var result = DictionaryExtensions.GetValueOrFallback(sut, key, fallback);
+
+            Assert.That(result, Is.EqualTo(value));
+        }
+
+        [Test, CustomAutoData]
+        public void Returns_fallback_value_if_key_is_not_available(string key, string fallback)
+        {
+            IDictionary<string, string> sut = new Dictionary<string, string>();
+
+            var result = DictionaryExtensions.GetValueOrFallback(sut, key, fallback);
+
+            Assert.That(result, Is.EqualTo(fallback));
+        }
+
+        [Test, CustomAutoData]
+        public void Returns_value_if_key_is_available_in_readonly_dictionary(string key, string value, string fallback)
+        {
+            IReadOnlyDictionary<string, string> sut = new Dictionary<string, string>
+            {
+                [key] = value
+            };
+
+            var result = DictionaryExtensions.GetValueOrFallback(sut, key, fallback);
+
+            Assert.That(result, Is.EqualTo(value));
+        }
+
+        [Test, CustomAutoData]
+        public void Returns_fallback_value_if_key_is_not_available_in_readonly_dictionary(string key, string fallback)
+        {
+            IReadOnlyDictionary<string, string> sut = new Dictionary<string, string>();
+
+            var result = DictionaryExtensions.GetValueOrFallback(sut, key, fallback);
+
+            Assert.That(result, Is.EqualTo(fallback));
+        }
+    }
+
+    [TestFixture]
+    [TestOf(nameof(DictionaryExtensions.GetValueOrFallback))]
+    public class GetValueOrDefault
+    {
+        [Test, CustomAutoData]
+        public void Returns_value_if_key_is_available_in_dictionary(string key, string value)
+        {
+            var sut = new Dictionary<string, string>
+            {
+                [key] = value
+            };
+
+            var result = DictionaryExtensions.GetValueOrDefault(sut, key);
+
+            Assert.That(result, Is.EqualTo(value));
+        }
+
+        [Test, CustomAutoData]
+        public void Returns_fallback_value_if_key_is_not_available_in_dictionary(string key)
+        {
+            var sut = new Dictionary<string, string>();
+
+            var result = DictionaryExtensions.GetValueOrDefault(sut, key);
+
+            Assert.That(result, Is.EqualTo(default(string)));
+        }
+
+        [Test, CustomAutoData]
+        public void Returns_value_if_key_is_available(string key, string value)
+        {
+            IDictionary<string, string> sut = new Dictionary<string, string>
+            {
+                [key] = value
+            };
+
+            var result = DictionaryExtensions.GetValueOrDefault(sut, key);
+
+            Assert.That(result, Is.EqualTo(value));
+        }
+
+        [Test, CustomAutoData]
+        public void Returns_fallback_value_if_key_is_not_available(string key)
+        {
+            IDictionary<string, string> sut = new Dictionary<string, string>();
+
+            var result = DictionaryExtensions.GetValueOrDefault(sut, key);
+
+            Assert.That(result, Is.EqualTo(default(string)));
+        }
+
+        [Test, CustomAutoData]
+        public void Returns_value_if_key_is_available_in_readonly_dictionary(string key, string value)
+        {
+            IReadOnlyDictionary<string, string> sut = new Dictionary<string, string>
+            {
+                [key] = value
+            };
+
+            var result = DictionaryExtensions.GetValueOrDefault(sut, key);
+
+            Assert.That(result, Is.EqualTo(value));
+        }
+
+        [Test, CustomAutoData]
+        public void Returns_fallback_value_if_key_is_not_available_in_readonly_dictionary(string key)
+        {
+            IReadOnlyDictionary<string, string> sut = new Dictionary<string, string>();
+
+            var result = DictionaryExtensions.GetValueOrDefault(sut, key);
+
+            Assert.That(result, Is.EqualTo(default(string)));
+        }
+    }
 }
